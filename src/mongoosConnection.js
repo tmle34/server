25c17afa52fd52cefc46a4fdd73594617d510696
tmle34 <55414823+tmle34@users.noreply.github.com>
2021-10-24 14:31:40 -0500

    connection

--- src/mongoosConnection.js
+++ src/mongoosConnection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose'),
-  url = 'mongodb://127.0.0.1:27017',
-  dbName = 'Users',
-  connectionString = url + '/' + dbName;
+  //url = 'mongodb://127.0.0.1:27017',
+  //dbName = 'Users',
+  //connectionString = url + '/' + dbName;
+  connectionString = 'mongodb+srv://thomas:thomas123@wbsserv.s3g0x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 mongoose.connect(connectionString);
 const db = mongoose.connection
